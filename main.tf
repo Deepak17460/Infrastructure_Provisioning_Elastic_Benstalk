45120052eba3f15a5c0f6ebf3278f5ff995dc07c
Deepak Singh <99780500+Deepak17460@users.noreply.github.com>
2024-07-11 10:50:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "elastic_beanstalk" {
   source              = "./modules/elastic-beanstalk"
-  app_name            = "tanisha-app"
-  env_name            = "tanisha-app-env"
-  domain              = "tanisha-app"
+  app_name            = "deepak-app"
+  env_name            = "deepak-app-env"
+  domain              = "deepak-app"
   solution_stack_name = "64bit Amazon Linux 2023 v5.1.7 running Tomcat 10 Corretto 17"
   instance_profile    = "tanisha-ec2-role"
   service_role        = "arn:aws:iam::876724398547:role/tanisha-ec2-role"
